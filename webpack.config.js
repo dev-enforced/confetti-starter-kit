de7e56fb55793c047f74b0acd2d60eafa81d5249
Roshan J <devenforced@gmail.com>
2022-10-03 13:47:34 +0530

    [feat]: HTML plugin for template generation attached

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require("path");
-
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 const webpackConfig = {
   mode: "production",
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "Confetti Kit Starter",
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
 };
 
 module.exports = webpackConfig;
